cb754f9ada559cef9eefd21efd8f04c8baa01b92
HarshitChakravarti <harshitchakravarti2006@gmail.com>
2025-09-26 22:58:27 +0530

    again testing payment gateway

--- src/app/api/create-razorpay-order/route.ts
+++ src/app/api/create-razorpay-order/route.ts
@@ -29,7 +29,7 @@ export async function POST(request: NextRequest) {
       }, { status: 400 });
     }
 
-    const key_id = process.env.RAZORPAY_KEY_ID;
+    const key_id = process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID;
     const key_secret = process.env.RAZORPAY_KEY_SECRET;
     if (!key_id || !key_secret) {
       return NextResponse.json({ 
