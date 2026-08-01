025d7a743376401cdeeae1d79aac03e4cc201334
HarshitChakravarti <harshitchakravarti2006@gmail.com>
2026-08-01 22:04:07 +0530

    meta pixel code

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import productReducer from './slices/productSlice';
+
 import cartReducer from './slices/cartSlice';
-import orderReducer from './slices/orderSlice';
 
 // Define the root state type
 const rootReducer = {
-  products: productReducer,
+  
   cart: cartReducer,
-  order: orderReducer,
+ 
 };
 
 // Create a function to make a new store
